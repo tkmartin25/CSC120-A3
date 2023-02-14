cf82333fe223127d6df7af63c96a334c03bf8a5c
Teddy Martin <tkmartin@smith.edu>
2023-02-14 18:53:59 -0500

    still trying to find out a way to replace words

--- Conversation.java
+++ Conversation.java
@@ -24,6 +24,7 @@ class Conversation {
     // storing how many times the user wants to talk
     rounds = user_rounds.nextInt();
     // while loop to talk user-specified number of times
+    String mirrored_response = "";
     while (counter <= rounds) {
       // counter incrementer
       counter++;
@@ -36,37 +37,35 @@ class Conversation {
       user_sentence = user_sentence.toLowerCase();
       // splitting to get rid of spaces
       String[] split = user_sentence.split(" ");
-      // make empty mirrored response
-      String mirrored_response = "";
-      // if any elemnts are mirror words, replace them and print new response with changes. Else, print ""
-      // if sentence contains mirrored words
-      for (String item : mirrored_words) {
-        if (user_sentence.contains(item)) {
-          for (String element : split) {
-            if (element.equals("i")) {
-              element.replace("i", "you");
-           // if etc.
-
-
-          
-           // add element to mirrored_response
+      // if any elemnts are mirror words, replace them and 
+      // print new response with changes. Else, print canned response
+      for (String element : split) {
+        for (String item : mirrored_words) {
+          if (element.equals(item)) {
+            if (element.equals("i")){
+              mirrored_response += "you ";
+            }
+          else {
+            mirrored_response += element + " ";
+            System.out.println(mirrored_response);
           }
-          mirrored_response += element + " ";
+          }
+        }
+      }
+      
+      if (mirrored_response.equals(user_sentence + " ")) {
+        // generate random number
+        Random random_number = new Random();
+        // print random canned response using random number generated above
+        System.out.println(canned_responses[random_number.nextInt(7)]); }
+
+      else {
+        System.out.println(mirrored_response);
+      }
+      
+    }
             
           
-          
-            // Print out changed response
-          }}}
-          System.out.println(mirrored_response);
-        // sentence does NOT contain mirrored words:
-        else {
-        // generate random number
-          Random random_number = new Random();
-        // print random canned response using random number generated above
-          System.out.println(canned_responses[random_number.nextInt(7)]);
-        }}
-    
-    
     
     user_rounds.close();
     userInput.close();
@@ -74,39 +73,6 @@ class Conversation {
     // final bye greeting
     System.out.println("Bye!");
   
-}}
-
-
-
-
-
-
-
-
-for (String element : split) {
-  for (String item : mirrored_words) {
-    if (element.equals(item)) {
-      if (element.equals("i")){
-        mirrored_response += "you ";
-      }
-    else {
-      mirrored_response += element + " ";
-
-    }
-    }
-  }
-}
-
-
-
-if (mirrored_response.equals(user_sentence)) {
-  // generate random number
-  Random random_number = new Random();
-  // print random canned response using random number generated above
-  System.out.println(canned_responses[random_number.nextInt(7)]);
-
-else {
-  System.out.println(mirrored_response);
   }
 }
   
