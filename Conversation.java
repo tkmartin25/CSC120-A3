8eaf473762b968ab7c7c8b2cd286b7e5ebd9be12
Teddy Martin <tkmartin@smith.edu>
2023-02-13 22:08:58 -0500

    random response out of canned responses array, as well as sources in reflection.md

--- Conversation.java
+++ Conversation.java
@@ -1,23 +1,44 @@
 import java.util.Scanner;
+import java.util.Random;
 
 class Conversation {
-
   public static void main(String[] arguments) {
     int rounds = 1;
-    int x = 1;
+    int counter = 1 ;
     String user_sentence = "";
+    String user_reply = "";
+    //array to hold replacements
+    String [] canned_responses = {"Mmm-hmm.", "Okay.", "Nice."};
+    String[][] replacements = {{"you ", "me "}, 
+                              {"i'm", "you are"},
+                              {"i am", "you are"},
+                              {"you are", "I am"},
+                              {"me ", "you "},
+                              {"your ", "my "},
+                              {"my ", "your "}
+                            };
 
     Scanner user_rounds = new Scanner(System.in);
     Scanner userInput = new Scanner(System.in);
+    Scanner userInput2 = new Scanner(System.in);
 
     System.out.println("Talk to me!");
     System.out.println("How many times do you want to talk to me? ");
     rounds = user_rounds.nextInt();
-    while (x <= rounds) {
-      x = x + 1;
+    while (counter <= rounds) {
+      counter++;
       System.out.print("Hi! Whatcha doin'? ");
       user_sentence = userInput.nextLine();
-      System.out.println(user_sentence.replace("I'm", "You are").replace("I am", "You are"));
+
+      //if (user_sentence.contains();
+      //loop over the array and replace
+      for(String[] replacement: replacements) {
+        user_sentence = user_sentence.replace(replacement[0], replacement[1]);
+      }
+      System.out.println(user_sentence);
+      user_reply = userInput2.nextLine();
+      Random random_number = new Random();
+      System.out.println(canned_responses[random_number.nextInt(3)]);
 
 
       
@@ -26,6 +47,7 @@ class Conversation {
     // You will start the conversation here.
     user_rounds.close();
     userInput.close();
+    userInput2.close();
     System.out.println("Bye!");
   }}
 
@@ -34,4 +56,6 @@ class Conversation {
 
 
 
+  
+  
   
--- reflection.md
+++ reflection.md
@@ -1,5 +1,7 @@
 Use this file to record your reflection on this assignment.
+https://www.educative.io/answers/how-to-generate-random-numbers-in-java
 
+https://stackoverflow.com/questions/6745154/how-to-replace-multiple-words-in-a-single-string-in-java 
 - Which classmates did you consult with while working on this assignment?
 - Which session(s) of TA / office hours did you attend?
 - What are your initial impressions of Java? 
