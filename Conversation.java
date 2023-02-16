f4090deef5a975c0386dbe061ea23fdc0da1ad70
Teddy Martin <tkmartin@smith.edu>
2023-02-15 19:27:51 -0500

    kind of got replacing words to work.

--- Conversation.java
+++ Conversation.java
@@ -8,11 +8,12 @@ class Conversation {
     int counter = 1 ;
     String user_sentence = "";
     String user_reply = ""; 
+    String modified_user_sentence = "";
     // array to hold responses 
     String [] transcript = {};
     // array to hold replacements
     String [] canned_responses = {"Mmm-hmm.", "Okay.", "Nice.", "I see.", "I get that.", "Aha.", "Real.", "It's so interesting to hear how humans think."};
-    String[] mirrored_words = {"you", "me", "am", "i", "my", "your"};
+    // list of mirrored words String[] mirrored_words = {"you", "me", "am", "i", "my", "your"};
 
     Scanner user_rounds = new Scanner(System.in);
     Scanner userInput = new Scanner(System.in);
@@ -24,57 +25,70 @@ class Conversation {
     // storing how many times the user wants to talk
     rounds = user_rounds.nextInt();
     // while loop to talk user-specified number of times
-    String mirrored_response = "";
     while (counter <= rounds) {
       // counter incrementer
       counter++;
+      String mirrored_response = "";
       // conversation initator
       System.out.print("Hi! Whatcha doin'? ");
       // user first response
       user_sentence = userInput.nextLine();
       // format user_sentence for analysis
-      // lowercase
-      user_sentence = user_sentence.toLowerCase();
+      modified_user_sentence = user_sentence.toLowerCase();
       // splitting to get rid of spaces
-      String[] split = user_sentence.split(" ");
+      String[] split = modified_user_sentence.split(" ");
       // if any elemnts are mirror words, replace them and 
       // print new response with changes. Else, print canned response
       for (String element : split) {
-        for (String item : mirrored_words) {
-          if (element.equals(item)) {
-            if (element.equals("i")){
-              mirrored_response += "you ";
-            }
-          else {
-            mirrored_response += element + " ";
-            System.out.println(mirrored_response);
-          }
-          }
+        if (element.equals("i")) {
+          element = "you";
         }
-      }
+        else if (element.equals("you")) {
+          element = "i";
+        }
+        else if (element.equals("my")) {
+          element = "your";
+        }
+        else if (element.equals("your")) {
+          element = "my";
+        }
+        else if (element.equals("me.")) {
+          element = "you";
+        }
+        else if (element.equals("am")) {
+          element = "are";
+        }
+        else if (element.equals("are")) {
+          element = "am";
+        }
+        else if (element.equals("you.")) {
+          element = "me";
+        }
+        mirrored_response += element + " ";
+        }
       
-      if (mirrored_response.equals(user_sentence + " ")) {
+      if (mirrored_response.equals(user_sentence.toLowerCase() + " ")) {
         // generate random number
         Random random_number = new Random();
         // print random canned response using random number generated above
-        System.out.println(canned_responses[random_number.nextInt(7)]); }
-
+        System.out.println(canned_responses[random_number.nextInt(7)]); 
+      }
       else {
         System.out.println(mirrored_response);
       }
       
-    }
+      
+  } // while loop bracket
             
           
-    
+  
     user_rounds.close();
     userInput.close();
     userInput2.close();
     // final bye greeting
     System.out.println("Bye!");
-  
-  }
 }
-  
-  
+}
+
+
   
